d1eaa5f333df948d6fb9d820f4d1437cbbbb20f1
Thomas <venturini.thomas95@gmail.com>
2022-01-31 23:08:03 +0100

    .

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,1 +1,14 @@
 //Consegna: Calcola la somma e la media dei primi 10 numeri.
+
+
+let sum = 0;
+let averageValue = 0;
+//ciclo for che parte da 1 e arriva a 10
+for (let i = 1; i < 11; i++) {
+    sum += i;
+    averageValue = sum / 10;
+
+}
+
+console.log("La somma dei primi 10 numeri è " + sum);
+console.log("La media dei primi 10 numeri è " + averageValue);
