7861e75e196de9d3bb1e9b4f461f766ab51219fb
Thomas <venturini.thomas95@gmail.com>
2022-01-31 17:53:02 +0100

    Es 1 completo

--- jsnack 1/main.js
+++ jsnack 1/main.js
@@ -1,1 +1,28 @@
-//Consegna: Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+/*Consegna: Il software deve chiedere per 10 volte
+ all’utente di inserire un numero. Il programma 
+ stampa la somma di tutti i numeri inseriti. */
+
+
+//dichiaro array e numero utente
+ let number;
+ let numbers= [];
+ let somma = 0;
+
+ //creo un ciclo che chiede 10 numeri all'utente
+
+ for (let i = 1; i <= 10; i++ ) {
+
+    number = parseInt(prompt(`Inserisci il numero in posizione ${i}`));
+    numbers.push(number);
+ }
+
+ console.log(numbers);
+
+ /* Fuori dal ciclo ho l'array e con
+  un ciclo ne faccio la somma degli elementi */
+
+for ( let i = 0; i < numbers.length; i++) {
+
+    somma += numbers[i];
+}
+console.log(`La somma è: `, somma);
